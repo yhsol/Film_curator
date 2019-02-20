98c38bf8faac6c9baf99e1d11c58f0a75585429f
yhsol <yhsol1592@gmail.con>
2019-02-20 20:37:47 +0900

    Updates

---
+++ precache-manifest.e315a0f5fc186b50464047e3648b3511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f31a6ce5aef7d8fdcbe4f1a9618a2a95",
+    "url": "/Film_curator/static/media/noPosterSmall.f31a6ce5.jpg"
+  },
+  {
+    "revision": "0ed7bfc6a30ef694f28d",
+    "url": "/Film_curator/static/js/runtime~main.0ed7bfc6.js"
+  },
+  {
+    "revision": "6f7d2210b82995be93d3",
+    "url": "/Film_curator/static/js/main.6f7d2210.chunk.js"
+  },
+  {
+    "revision": "79d2d1ea5d91f2e4e900",
+    "url": "/Film_curator/static/js/1.79d2d1ea.chunk.js"
+  },
+  {
+    "revision": "9cf6e8ec8a62d9b8962562098721f700",
+    "url": "/Film_curator/index.html"
+  }
+];
